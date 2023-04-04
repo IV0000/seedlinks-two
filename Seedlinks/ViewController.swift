41bf8c8c2c75904f38c7caa24eec8b12023f1172
Ivan Voloshchuk <94223094+IV0000@users.noreply.github.com>
2023-04-04 08:10:57 +0200

    Project setup and testing CI (#1)
    * Add swiftlint action
    
    * Update swiftlint.yml
    
    * Update and rename swiftlint.yml to formatting-linting.yml
    
    * Initial Commit
    
    * Add swiftformat
    
    * Downgrade swiftformat version
    
    * Upgrade swiftformat
    
    * Swiftformat again update
    
    * Add linting rules
    
    * Update formatting-linting.yml
    
    * Update swiftformat script
    
    * Fix swiftlint warnings
    
    * Update swiftlint to 0.51.0
    
    * Add ds store to gitignore
    
    * Remove ds_store
    
    * Change bundle id

---
+++ Seedlinks/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Seedlinks
+//
+//  Created by Ivan Voloshchuk on 02/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
