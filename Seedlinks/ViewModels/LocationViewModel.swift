e2e7319a510b43dd9d11d029e80de586d6c3ef56
Ivan Voloshchuk <keranamv@gmail.com>
2023-04-27 11:34:30 +0200

    Add location view model functions

--- Seedlinks/ViewModels/LocationViewModel.swift
+++ Seedlinks/ViewModels/LocationViewModel.swift
@@ -11,7 +11,35 @@ import Foundation
 import MapKit
 
 class LocationViewModel {
-    func getLastLocation() -> AnyPublisher<CLLocation, Error> {
-        LocationManager.shared.traceLocation()
+    public func getCurrentLocation() -> AnyPublisher<CLLocation, CLError> {
+        guard !LocationManager.shared.isLocationDenied() else {
+            return Fail(error: CLError(.denied)).eraseToAnyPublisher()
+        }
+        return LocationManager.shared.requestLocation()
+    }
+
+    public func requestWhenInUseAuthorization() -> AnyPublisher<Bool, CLError> {
+        guard LocationManager.shared.isLocationEnabled() else {
+            return Fail(error: CLError(.locationUnknown)).eraseToAnyPublisher()
+        }
+
+        guard !LocationManager.shared.isLocationDenied() else {
+            return Just(false)
+                .setFailureType(to: CLError.self)
+                .eraseToAnyPublisher()
+        }
+
+        return LocationManager.shared.requestWhenInUseAuthorization()
+            .filter { $0 != .notDetermined }
+            .first()
+            .map { _ in LocationManager.shared.isLocationAuthorized() }
+            .catch { error in
+                if error == CLError(.denied) {
+                    return Just(false).setFailureType(to: CLError.self).eraseToAnyPublisher()
+                } else {
+                    return Fail(error: CLError(.denied)).eraseToAnyPublisher()
+                }
+            }
+            .eraseToAnyPublisher()
     }
 }
